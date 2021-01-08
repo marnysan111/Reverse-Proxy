213e1ba51bdc2ef6eb4e3068612a35b97c011f66
marnysan111 <o-masato@outlook.jp>
2021-01-08 15:44:24 +0900

    ファイルの整理

--- cpu/main.go
+++ cpu/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "cpu/write"
+import (
+	"cpu/manage"
+	"cpu/write"
+	"net/http"
+)
 
 func main() {
 	ip := "192.168.56.1"
 	port := "50000"
 	write.Write(ip, port)
+
+	http.HandleFunc("/", manage.Top)
+	http.ListenAndServe(":8080", nil)
 }
